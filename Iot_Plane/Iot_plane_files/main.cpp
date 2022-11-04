0e999b28d7609f57e92a96447f29d8b9fad27969
BlacKoder <nanmegningassam@gmail.com>
2022-11-04 12:49:16 +0100

    Initialization of our project repository

---
+++ Iot_Plane/Iot_plane_files/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @author NANMEGNI_NGASSAM
+ * @file main.cpp
+ * @date 03/11/2022
+ * @brief That's the file where all the modules will be included and implemented in order to realize our Iot_Plane
+ */
+
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main()
+{
+
+  return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Iot_Plane
-C++ Project : Iot_Plane console application
+**C++ Project :** Iot_Plane console application
+**Purposes :**
+* stores relevant data from a plane during a fly
+* displays data stored from a past fly 
